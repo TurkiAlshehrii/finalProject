1667aa1215b7a217854a20d902ee944ff35db98b
TurkiAlshehrii <alshhryt547+1@gmail.com>
2022-08-13 03:43:01 +0300

    add images and use recyclerView and cardView

--- app/src/main/java/com/example/project_2_recyclerview_app/MainActivity.kt
+++ app/src/main/java/com/example/project_2_recyclerview_app/MainActivity.kt
@@ -2,13 +2,25 @@ package com.example.project_2_recyclerview_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_2_recyclerview_app.adapter.restaurantAdapter
+import com.example.project_2_recyclerview_app.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Connect the adapter with the data here
-        // Bind the recyclerview and the adapter here
+        // Data source
+        val resturntList = Datasource().loadMune()
+
+        // adapter
+        val adapter = restaurantAdapter(resturntList, this)
+
+        // recyclerView
+        val recyclerView : RecyclerView = findViewById(R.id.rv_resturnt)
+
+        recyclerView.adapter = adapter
+        recyclerView.setHasFixedSize(true)
     }
 }
